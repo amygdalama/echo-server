ab9e9f43baac6ad46abab644aed040a2693fa916
Amy Hanlon <amyehanlon@gmail.com>
2015-01-17 20:21:22 -0500

    initial commit

---
+++ server.py
@@ -0,0 +1,35 @@
+from twisted.internet import reactor
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet.endpoints import TCP4ServerEndpoint
+
+
+class Echo(Protocol):
+
+    def __init__(self, factory):
+        self.factory = factory
+
+    def connectionMade(self):
+        self.factory.numProtocols += 1
+        self.transport.write(
+            "Welcome! There are currently %d open connections." %
+            self.factory.numProtocols)
+
+    def connectionLost(self, reason):
+        self.factory.numProtocols -= 1
+
+    def dataReceived(self, data):
+        self.transport.write(data)
+
+
+class EchoFactory(Factory):
+
+    protocol = Echo
+
+    def __init__(self):
+        self.numProtocols = 0
+
+
+if __name__ == '__main__':
+    endpoint = TCP4ServerEndpoint(reactor, 8007)
+    endpoint.listen(EchoFactory())
+    reactor.run()
